3aed1f56adcab232faa0d9b895ef0763018ecb73
KIRAN KUMHAR <kiran.kumar91@live.com>
2024-12-13 00:03:44 +0530

    task completed event

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 use App\Http\Controllers\TaskController;
 
 Route::get('/tasks-all', [TaskController::class, 'getTasks']);
+Route::post('/tasks-all/completed/{id}', [TaskController::class, 'markAsCompleted']);
